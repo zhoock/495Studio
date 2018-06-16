2818f7fb5efa5c6c63ce047cff660097622a4151
Yaroslav Zhoock <zhoock@zhoock.ru>
2018-06-17 00:47:55 +0500

    добавил uglify js в gruntfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,139 +1,139 @@
 "use strict";
 
 var gulp            = require('gulp'),
-    compass         = require('gulp-compass'),
-    minifyCSS       = require('gulp-minify-css'),
-    rename          = require("gulp-rename"),
-    jade            = require('gulp-jade'),
-    watch           = require('gulp-watch'),
-    notify          = require("gulp-notify"),
-    livereload      = require('gulp-livereload'),
-    connect         = require('gulp-connect'),
-    rev             = require('gulp-rev'),
-    revCollector    = require('gulp-rev-collector'),
-    gutil           = require('gulp-util'),
-    rimraf          = require('rimraf'),
-    revOutdated     = require('gulp-rev-outdated'),
-    path            = require('path'),
-    through         = require('through2'),
-    uglify          = require('gulp-uglify'),
-    pump            = require('pump'),
-    jsImport        = require('gulp-js-import');
+	compass         = require('gulp-compass'),
+	minifyCSS       = require('gulp-minify-css'),
+	rename          = require("gulp-rename"),
+	jade            = require('gulp-jade'),
+	watch           = require('gulp-watch'),
+	notify          = require("gulp-notify"),
+	livereload      = require('gulp-livereload'),
+	connect         = require('gulp-connect'),
+	rev             = require('gulp-rev'),
+	revCollector    = require('gulp-rev-collector'),
+	gutil           = require('gulp-util'),
+	rimraf          = require('rimraf'),
+	revOutdated     = require('gulp-rev-outdated'),
+	path            = require('path'),
+	through         = require('through2'),
+	uglify          = require('gulp-uglify'),
+	pump            = require('pump'),
+	jsImport        = require('gulp-js-import');
+
 
 // server connect
 gulp.task('connect', function () { 
-    connect.server({
-        port: 1610,
-        root: 'www',
-        livereload: true
-    });
-});
-
-// compress js
-gulp.task('compress', function () {
-  pump([
-        gulp.src('./app/js/main.js'),
-        uglify(),
-        gulp.dest('./www/js/')
-    ],
-  );
-});
-
-// import js
-gulp.task('import', function() {
-  return gulp.src('./app/js/main.js')
-        .pipe(jsImport({hideConsole: true}))
-        .pipe(gulp.dest('./www/js/'));
-});
-
-// gulp.task('compress', function () {
-//     gulp.src('./app/js/main.js')
-//         .pipe(uglify())
-//         .pipe(gulp.dest('./www/'));
-// });
-
-
-// templates
-gulp.task('templates', function () {
-    gulp.src('./app/jade/*.jade')
-        .pipe(jade({
-            pretty: true,
-            data: {
-                debug: false
-            }
-        }))
-        .pipe(gulp.dest('./www/'))
-        .pipe(connect.reload());
+	connect.server({
+		port: 1610,
+		root: 'www',
+		livereload: true
+	});
 });
 
 
+// jade
+gulp.task('jade', function () {
+	gulp.src('./app/jade/*.jade')
+		.pipe(jade({
+			pretty: true,
+			data: {
+				debug: false
+			}
+		}))
+		.pipe(gulp.dest('./www/'))
+		.pipe(connect.reload());
+});
+
+// compass
 gulp.task('rev', function () {
-    return gulp.src('./app/**/*.sass')
-        .pipe(compass({
-            config_file: './config.rb',
-            css: 'www/css',
-            sass: 'app/sass',
-            sourcemap: true
-        }))
-        .on('error', function (err) {
-            console.log(err);
-            this.emit('end');
-        })
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('./www/css/'))
+	return gulp.src('./app/**/*.+(scss|sass)')
+		.pipe(compass({
+			config_file: './config.rb',
+			css: 'www/css',
+			sass: 'app/sass',
+			sourcemap: true
+		}))
+		.on('error', function (err) {
+			console.log(err);
+			this.emit('end');
+		})
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('./www/css/'))
 
 
-        .pipe(rev())
-        .pipe(gulp.dest('./www/css/'))
-        .pipe(rev.manifest())
-        .pipe(gulp.dest('./app/manifests/'))
+		.pipe(rev())
+		.pipe(gulp.dest('./www/css/'))
+		.pipe(rev.manifest())
+		.pipe(gulp.dest('./app/manifests/'))
 
-        .pipe(notify('Done!'))
+		.pipe(notify('Done!'))
 
 });
 
 gulp.task('rev_collector', ['rev'], function () {
-    return gulp.src(['./app/manifests/**/*.json', './www/*.html'])
-        .pipe(revCollector({
-            replaceReved: true
-        }))
+	return gulp.src(['./app/manifests/**/*.json', './www/*.html'])
+		.pipe(revCollector({
+			replaceReved: true
+		}))
 
-        .pipe(gulp.dest('./www/'))
-        .pipe(connect.reload())
+		.pipe(gulp.dest('./www/'))
+		.pipe(connect.reload())
 });
 
 function cleaner() {
-    return through.obj(function (file, enc, cb) {
-        rimraf(path.resolve((file.cwd || process.cwd()), file.path), function (err) {
-            if (err) {
-                this.emit('error', new gutil.PluginError('Cleanup old files', err));
-            }
-                      cb();
-        }.bind(this));
-    });
+	return through.obj(function (file, enc, cb) {
+		rimraf(path.resolve((file.cwd || process.cwd()), file.path), function (err) {
+			if (err) {
+				this.emit('error', new gutil.PluginError('Cleanup old files', err));
+			}
+					  cb();
+		}.bind(this));
+	});
 }
 
 gulp.task('clean',['rev_collector'], function () {
-    gulp.src(['./www/**/*.*'], {read: false})
-        .pipe(revOutdated(1)) // leave 2 recent assets (default value)
-        .pipe(cleaner())
+	gulp.src(['./www/**/*.*'], {read: false})
+		.pipe(revOutdated(1)) // leave 2 recent assets (default value)
+		.pipe(cleaner())
 
-    return;
+	return;
 
 }); 
 
 
-// watch
-gulp.task('watch', function () {
-    gulp.watch(('app/**/*.jade'), ['templates']);
-    gulp.watch(('app/**/*.sass'), ['rev_all']);
-    gulp.watch(('app/**/*.js'), ['compress']);
+//uglify js
+gulp.task('uglify', function () {
+	pump([
+		gulp.src('./www/js/main.js'),
+		uglify(),
+		gulp.dest('./www/js')
+		],
+	);
 });
 
-// default
-gulp.task('default', ['connect', 'templates', 'rev_all', 'import', 'compress', 'watch']);
 
-// rev_all
-gulp.task('rev_all', ['rev', 'rev_collector', 'clean']);
+// import js
+gulp.task('javascript', function() {
+  return gulp.src('./app/js/main.js')
+		.pipe(jsImport({hideConsole: true}))
+		.pipe(uglify())
+		.pipe(gulp.dest('./www/js'))
+
+});
+
+
+// watch
+gulp.task('watch', function () {
+	gulp.watch(('./app/**/*.jade'), ['jade']);
+	gulp.watch(('./app/**/*.+(scss|sass)'), ['compass']);
+	gulp.watch(('./app/**/*.js'), ['javascript']);
+});
+
+
+// default
+gulp.task('default', ['connect', 'jade', 'compass', 'javascript', 'watch']);
+
+// compass
+gulp.task('compass', ['rev', 'rev_collector', 'clean']);
 
 
