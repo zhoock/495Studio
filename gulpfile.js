a47b4e56b6e817dfc9c3436dec9fc5b4cfc55279
Yaroslav Zhoock <zhoock@zhoock.ru>
2018-06-20 01:16:02 +0500

    добавил файлы для сборки

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,38 @@
 "use strict";
 
-var gulp            = require('gulp'),
-	compass         = require('gulp-compass'),
-	minifyCSS       = require('gulp-minify-css'),
-	rename          = require("gulp-rename"),
-	jade            = require('gulp-jade'),
-	watch           = require('gulp-watch'),
-	notify          = require("gulp-notify"),
-	livereload      = require('gulp-livereload'),
-	connect         = require('gulp-connect'),
-	rev             = require('gulp-rev'),
-	revCollector    = require('gulp-rev-collector'),
-	gutil           = require('gulp-util'),
-	rimraf          = require('rimraf'),
-	revOutdated     = require('gulp-rev-outdated'),
-	path            = require('path'),
-	through         = require('through2'),
-	uglify          = require('gulp-uglify'),
-	pump            = require('pump'),
-	jsImport        = require('gulp-js-import');
+var gulp = require('gulp'),
+	compass = require('gulp-compass'),
+	minifyCSS = require('gulp-minify-css'),
+	rename = require("gulp-rename"),
+	jade = require('gulp-jade'),
+	watch = require('gulp-watch'),
+	notify = require("gulp-notify"),
+	livereload = require('gulp-livereload'),
+	connect = require('gulp-connect'),
+	rev = require('gulp-rev'),
+	revCollector = require('gulp-rev-collector'),
+	gutil = require('gulp-util'),
+	rimraf = require('rimraf'),
+	revOutdated = require('gulp-rev-outdated'),
+	path = require('path'),
+	through = require('through2'),
+	uglify = require('gulp-uglify'),
+	pump = require('pump'),
+	jsImport = require('gulp-js-import');
 
 
 // server connect
-gulp.task('connect', function () { 
+gulp.task('connect', function() {
 	connect.server({
 		port: 1610,
-		root: 'www',
+		root: 'public_html',
 		livereload: true
 	});
 });
 
 
 // jade
-gulp.task('jade', function () {
+gulp.task('jade', function() {
 	gulp.src('./app/jade/*.jade')
 		.pipe(jade({
 			pretty: true,
@@ -40,29 +40,29 @@ gulp.task('jade', function () {
 				debug: false
 			}
 		}))
-		.pipe(gulp.dest('./www/'))
+		.pipe(gulp.dest('./public_html/'))
 		.pipe(connect.reload());
 });
 
 // compass
-gulp.task('rev', function () {
+gulp.task('rev', function() {
 	return gulp.src('./app/**/*.+(scss|sass)')
 		.pipe(compass({
 			config_file: './config.rb',
-			css: 'www/css',
-			sass: 'app/sass',
+			css: './public_html/css',
+			sass: './app/sass',
 			sourcemap: true
 		}))
-		.on('error', function (err) {
+		.on('error', function(err) {
 			console.log(err);
 			this.emit('end');
 		})
 		.pipe(minifyCSS())
-		.pipe(gulp.dest('./www/css/'))
+		.pipe(gulp.dest('./public_html/css/'))
 
 
 		.pipe(rev())
-		.pipe(gulp.dest('./www/css/'))
+		.pipe(gulp.dest('./public_html/css/'))
 		.pipe(rev.manifest())
 		.pipe(gulp.dest('./app/manifests/'))
 
@@ -70,62 +70,62 @@ gulp.task('rev', function () {
 
 });
 
-gulp.task('rev_collector', ['rev'], function () {
-	return gulp.src(['./app/manifests/**/*.json', './www/*.html'])
+gulp.task('rev_collector', ['rev'], function() {
+	return gulp.src(['./app/manifests/**/*.json', './public_html/*.html'])
 		.pipe(revCollector({
 			replaceReved: true
 		}))
 
-		.pipe(gulp.dest('./www/'))
+		.pipe(gulp.dest('./public_html/'))
 		.pipe(connect.reload())
 });
 
 function cleaner() {
-	return through.obj(function (file, enc, cb) {
-		rimraf(path.resolve((file.cwd || process.cwd()), file.path), function (err) {
+	return through.obj(function(file, enc, cb) {
+		rimraf(path.resolve((file.cwd || process.cwd()), file.path), function(err) {
 			if (err) {
 				this.emit('error', new gutil.PluginError('Cleanup old files', err));
 			}
-					  cb();
+			cb();
 		}.bind(this));
 	});
 }
 
-gulp.task('clean',['rev_collector'], function () {
-	gulp.src(['./www/**/*.*'], {read: false})
+gulp.task('clean', ['rev_collector'], function() {
+	gulp.src(['./public_html/**/*.*'], { read: false })
 		.pipe(revOutdated(1)) // leave 2 recent assets (default value)
 		.pipe(cleaner())
 
 	return;
 
-}); 
+});
 
 
 //uglify js
-gulp.task('uglify', function () {
+gulp.task('uglify', function() {
 	pump([
-		gulp.src('./www/js/main.js'),
+		gulp.src('./public_html/js/main.js'),
 		uglify(),
-		gulp.dest('./www/js')
-		],
-	);
+		gulp.dest('./public_html/js')
+	], );
 });
 
 
 // import js
 gulp.task('javascript', function() {
-  return gulp.src('./app/js/main.js')
-		.pipe(jsImport({hideConsole: true}))
+	return gulp.src('./app/js/main.js')
+		.pipe(jsImport({ hideConsole: true }))
 		.pipe(uglify())
-		.pipe(gulp.dest('./www/js'))
+		.pipe(gulp.dest('./public_html/js'))
+		.pipe(connect.reload());
 
 });
 
 
 // watch
-gulp.task('watch', function () {
-	gulp.watch(('./app/**/*.jade'), ['jade']);
-	gulp.watch(('./app/**/*.+(scss|sass)'), ['compass']);
+gulp.task('watch', function() {
+			gulp.watch(('./app/**/*.jade'), ['jade']);
+			gulp.watch(('./app/**/*.+(scss|sass)'), ['compass']);
 	gulp.watch(('./app/**/*.js'), ['javascript']);
 });
 
@@ -135,5 +135,3 @@ gulp.task('default', ['connect', 'jade', 'compass', 'javascript', 'watch']);
 
 // compass
 gulp.task('compass', ['rev', 'rev_collector', 'clean']);
-
-
